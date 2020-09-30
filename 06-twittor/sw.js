e5bb7b9dcdd10e64e466049be296b94db1d99147
Andrés López Atehortua <andres.lopez.ate@gmail.com>
2020-10-01 00:39:22 +0200

    adaptacion para ios implementada

--- 06-twittor/sw.js
+++ 06-twittor/sw.js
@@ -1,7 +1,7 @@
 // imports
 importScripts('/js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 
@@ -60,32 +60,5 @@ self.addEventListener('fetch', (e) => {
           )
     );
 
-  // const respuesta = caches.match(e.request).then((resp) => {
-  //   if (resp) {
-  //     return resp;
-  //   } else {
-  //     return fetch(e.request).then((newResp) => {
-  //       return saveDinamicCache(DYNAMIC_CACHE, e.request, newResp);
-  //     });
-  //   }
-  // });
-
   e.respondWith(cacheResponse);
 });
-
-//   const cacheResponse = caches.match(e.request).then((res) => {
-//     if (res) return res;
-//     // Aquí trabajamos si no existe los archivos en la caché por lo que hay que ir a la web
-//     return fetch(e.request).then((newRes) => {
-//       // Aquí ha logrado encontrar los archivos con la red
-//       // Guardamos entonces los archivos conseguidos por la red en la caché
-//       caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
-//         cache.put(e.request, newRes);
-//         // Con esta función controlamos el límite de archivos guardados en la caché
-//         clearCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
-//       });
-//       // Hacemos un clon de la respuesta ya que no se puede usar dos veces
-//       return newRes.clone();
-//     });
-//   });
-//   e.respondWith(cacheResponse);
